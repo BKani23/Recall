909866ee7bd39a5e7b95850ca74e91baa7479863
BKani23 <bkani2304@gmail.com>
2026-06-21 12:38:11 +0200

    feat: integrate database and routes into server setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+import noteRoutes from "./routes/noteRoutes.js";
 
 // Load environment variables
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,6 +16,9 @@ app.use(express.json());
 
 // Get port from environment variables
 const PORT = process.env.PORT || 3000;
+
+// Routes
+app.use("/api/notes", noteRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT: ${PORT}`);
