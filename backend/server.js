30be64e6bafed85dd9f7777942a485111652a878
BKani23 <bkani2304@gmail.com>
2026-06-21 11:49:44 +0200

    chore: add dotenv configuration for server port

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
 
 const app = express();
 
-//Middleware 
-
+// Middleware
 app.use(express.json());
 
-const PORT = 3000;
+// Get port from environment variables
+const PORT = process.env.PORT || 3000;
 
-
-app.listen(3000,()=>{
-
-     console.log("Server started on PORT : " + PORT);
-     
-})
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
