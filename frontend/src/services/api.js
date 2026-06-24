e5464b71f0991fc2fdf72383bafc0389d9def276
BKani23 <bkani2304@gmail.com>
2026-06-24 15:34:21 +0200

    feat: create axios-based service for creating, reading, updating, and deleting notes

---
+++ frontend/src/services/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+// Base URL of  backend
+const API = axios.create({
+  baseURL: "http://localhost:3000/api/notes",
+});
+
+export const createNote = (data) => API.post("/", data);
+
+export const getNotes = () => API.get("/");
+
+export const getNoteById = (id) => API.get(`/${id}`);
+
+export const updateNote = (id, data) => API.put(`/${id}`, data);
+
+export const deleteNote = (id) => API.delete(`/${id}`);
